3df7352dd48680a90ee99633ccc3dd4812f7b5aa
Satish Motukuri (CW) <satish.motukuri@wdc.com>
2023-05-03 12:19:08 +0530

    Added python programs file to modify given string

---
+++ python_programs1.py
@@ -0,0 +1,33 @@
+"""
+    # @file                : python_programs1.py
+    # Program Description  : python programs to modify existing string
+    # Date         : 3rd May 2023
+"""
+
+class pythonPrograms():
+    """
+    Different Python Programs on strings
+    """
+    def __init__(self, string):
+        self.string = string
+        self.modified_string = ""
+
+    def remove_space_from_string(self):
+        """
+        This function removes spaces from a given sentence
+        :return:
+        """
+        for each_char in self.string:
+            if each_char == " ":
+                print("space removed")
+            else:
+                self.modified_string+=each_char
+        return self.modified_string
+
+classobj = pythonPrograms("Hello World")
+output = classobj.remove_space_from_string()
+print(output)
+
+
+
+
